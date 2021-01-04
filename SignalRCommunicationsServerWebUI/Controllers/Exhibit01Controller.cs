4a925a174c9f8b415eb3aa1862748abe7f46baeb
Olaaf Rossi <olaaf.rossi@gmail.com>
2021-01-04 10:59:46 +0700

    adds a models folder and an Exhibit01 controller

---
+++ SignalRCommunicationsServerWebUI/Controllers/Exhibit01Controller.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRCommunicationsServerWebUI.Models
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Exhibit01Controller : ControllerBase
+    {
+        private readonly ILogger<Exhibit01Controller> _logger;
+
+        public Exhibit01Controller(ILogger<Exhibit01Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Exhibit01> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 1).Select(index => new Exhibit01
+                {
+                    Message = "hi",
+                    Number = rng.Next(1,100)
+                })
+                .ToArray();
+        }
+    }
+}
---
+++ SignalRCommunicationsServerWebUI/Models/Exhibit01.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRCommunicationsServerWebUI.Models
+{
+    public class Exhibit01
+    {
+        public string Message { get; set; }
+
+        public int Number { get; set; } 
+    }
+}
